aa137530e5da3a58b67207846658766abe7abaf0
Sergey Yacovlev <sergio.yacovlev@outlook.com>
2018-01-21 19:58:27 +0200

    Repos.

---
+++ Catalog.Repo/MetaCategoriesRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Catalog.Models;
+
+namespace Catalog.Repo {
+
+    public interface MetaCategoriesRepo {
+
+        Task<IList<MetaCategory>> All();
+
+        Task<bool> Register(
+            MetaCategory metaCategory);
+
+        Task<bool> Unregiester(
+            MetaCategory metaCategory);
+
+    }
+
+}
---
+++ Catalog.Repo/ProductsRepo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Catalog.Models;
+
+namespace Catalog.Repo {
+
+    public interface ProductsRepo {
+
+        Task<IList<Product>> AllByMetacategory(
+            int metaCategoryID);
+
+        Task<IList<Product>> AllByMetacategoryAndProductKind(
+            int metaCategoryID,
+            int productKindID);
+
+    }
+
+}
