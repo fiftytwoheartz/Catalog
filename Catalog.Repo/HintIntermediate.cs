d3afd12b71912c7554de129676c8923bb5c0fef6
sgrechko <sgrechko@DT-TAL-000785>
2018-01-23 11:12:38 +0200

    HintIntermediate, SuccessfulResponse.

---
+++ Catalog.Repo/HintIntermediate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Catalog.Repo
+{
+	public class HintIntermediate
+	{
+		public bool StaysForSuccess;
+
+		public bool StaysForFailure;
+
+		public string ActionDescription;
+
+		public string RelativeURL;
+
+		public HintIntermediate(bool staysForSuccess, bool staysForFailure, string actionDescription, string relativeURL)
+		{
+			StaysForSuccess = staysForSuccess;
+			StaysForFailure = staysForFailure;
+			ActionDescription = actionDescription;
+			RelativeURL = relativeURL;
+		}
+
+		public Hint Build()
+		{
+			return new Hint(ActionDescription, RelativeURL);
+		}
+	}
+
+	public class Hint
+	{
+		public readonly string ActionDescription;
+
+		public readonly string RelativeURL;
+
+		public Hint(string actionDescription, string relativeUrl)
+		{
+			ActionDescription = actionDescription;
+			RelativeURL = relativeUrl;
+		}
+	}
+}
---
+++ Catalog.Repo/SuccessfulResponse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Catalog.Repo {
+
+    public abstract class Response {
+
+        public readonly bool Success;
+
+	    public IEnumerable<Hint> Hints;
+
+        protected Response(
+            bool success,
+			IEnumerable<Hint> hints) {
+            Success = success;
+			Hints = hints ?? Enumerable.Empty<Hint>();
+        }
+
+    }
+
+	public sealed class FailedResponse : Response
+	{
+		public readonly string ErrorMessage;
+
+		public FailedResponse(string errorMessage, IEnumerable<Hint> hints)
+			: base(false, hints)
+		{
+			ErrorMessage = errorMessage;
+		}
+	}
+
+    public sealed class SuccessfulResponse<T> : Response {
+
+        public readonly T Data;
+
+        public SuccessfulResponse(
+			IEnumerable<Hint> hints, 
+			T data) : base(true, hints) {
+            Data = data;
+        }
+
+    }
+
+}
