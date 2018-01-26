c55a499cb61c230b411786613ad3bcf2d6b79d8b
sgrechko <sgrechko@DT-TAL-000785>
2018-01-26 14:47:22 +0200

    Admin panel.

---
+++ Catalog.WebSite/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Catalog.WebSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
